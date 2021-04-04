ac1785f9fc563131a25e0f985d684b7eddc6ec9a
GirkovArpa <girkovarpa@protonmail.com>
2021-04-03 22:51:45 -0500

    :sparkles: Validation des entrées de l'organisateur

--- organisateur/src/my_window.rs
+++ organisateur/src/my_window.rs
@@ -1,20 +1,26 @@
 use winsafe::gui;
 use winsafe::WinResult;
 
+use chrono::{NaiveDate, Datelike};
+
 #[derive(Clone)]
 pub struct MyWindow {
   wnd: gui::WindowMain,
   btn: gui::Button,
-  cmb: gui::ComboBox
+  cmb: gui::ComboBox,
+  one: gui::Edit,
+  ret: gui::Edit
 }
 
 impl MyWindow {
   pub fn new() -> MyWindow {
     let wnd = gui::WindowMain::new_dlg(1000, Some(101), None);
-    let btn = gui::Button::new_dlg(&wnd, 2);
-    let cmb = gui::ComboBox::new_dlg(&wnd, 3);
+    let cmb = gui::ComboBox::new_dlg(&wnd, 0);
+    let one = gui::Edit::new_dlg(&wnd, 1);
+    let ret = gui::Edit::new_dlg(&wnd, 2);
+    let btn = gui::Button::new_dlg(&wnd, 3);
     
-    let myself = Self { wnd, cmb, btn };
+    let myself = Self { wnd, cmb, btn, one, ret };
     myself.events();
     myself
   }
@@ -34,6 +40,63 @@ impl MyWindow {
         }
     });
 
+    self.cmb.on().cbn_sel_change({
+      let wnd = self.clone();
+
+      move || {
+        let selection = wnd.cmb.items().selected_text().unwrap();
+        wnd.ret.hwnd().EnableWindow(selection != "one-way flight");
+
+        wnd.btn.hwnd().EnableWindow(validate(wnd.to_owned()));
+      }
+    });
+
+    self.one.on().en_change({
+      let wnd = self.clone();
+      move || {
+        wnd.btn.hwnd().EnableWindow(validate(wnd.to_owned()));
+      }
+    });
+
+    self.ret.on().en_change({
+      let wnd = self.clone();
+      move || {
+        wnd.btn.hwnd().EnableWindow(validate(wnd.to_owned()));
+      }
+    });
+
   }
 
+}
+
+fn validate(wnd: MyWindow) -> bool {
+    let cmb = wnd.cmb;
+    let one = wnd.one;
+    let ret = wnd.ret;
+
+    // if either date is invalid, return false
+    let date_string_one = one.text().unwrap();
+    let date_struct_one = match NaiveDate::parse_from_str(&date_string_one, "%d.%m.%Y") {
+      Ok(val) => val,
+      Err(_) => return false
+    };
+    let date_days_one = date_struct_one.num_days_from_ce();
+
+    // validation of second edit if return flight is selected
+    let selection = cmb.items().selected_text().unwrap();
+    if selection == "return flight" {
+      let date_string_ret = ret.text().unwrap();
+      let date_struct_ret = match NaiveDate::parse_from_str(&date_string_ret, "%d.%m.%Y") {
+        Ok(val) => val,
+        Err(_) => return false
+      };
+      let date_days_ret = date_struct_ret.num_days_from_ce();
+
+      // if start date is after return date ;)
+      if date_days_one > date_days_ret {
+        return false;
+      }
+    }
+
+    true
 }
